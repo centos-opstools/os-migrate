2b193e2d707651672a5b534224a464cc659eda56
Lars Kellogg-Stedman <lars@redhat.com>
2017-12-01 14:44:07 -0500

    documented driver interface

--- os_migrate/ds/base.py
+++ os_migrate/ds/base.py
@@ -13,3 +13,23 @@ class Datasource(object):
         self.log = logging.getLogger('{}.{}'.format(
             self.__module__,
             self.__class__.__name__))
+
+    @abc.abstractproperty
+    def description(self):
+        '''The `description` property should return a short
+        description of the driver.'''
+        pass
+
+    @abc.abstractmethod
+    def store(self):
+        '''The `store` method will return a dictionary of resources.
+        The contents of the dictionary are intended to be consumed by
+        the `load` method.'''
+        pass
+
+    @abc.abstractmethod
+    def load(self, resources):
+        '''The `load` method will instantiate resources based on the
+        contents of the `resources` dictionary, which was previously
+        created by the `store` method.'''
+        pass
